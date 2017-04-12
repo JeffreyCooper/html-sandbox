83fb4f79bf0082ae7c1e264417489a50bdd773db
Jeffrey Cooper <jcooper37@cnm.edu>
2017-04-12 08:30:16 -0600

    Add JS script file

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 		<title>HTML Sandbox</title>
 		<!-- Custom CSS -->
 		<link rel="stylesheet" href="css/sandbox-style.css" type="text/css" />
+<!--		Custom JavaScript-->
+		<script type="text-javascript" src="js/sandbox-scripts.js"></script>
 	</head>
 	<body>
 		<h1>HTML Sandbox</h1>
