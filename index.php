f317cc095bf6bc0f018c48c37e8c7e7257527a8a
Jeffrey Cooper <jcooper37@cnm.edu>
2017-04-12 08:54:18 -0600

    Add JS script file

--- index.php
+++ index.php
@@ -9,6 +9,19 @@
 		<script type="text-javascript" src="js/sandbox-scripts.js"></script>
 	</head>
 	<body>
-		<h1>HTML Sandbox</h1>
+		<header>
+			<h1>HTML Sandbox</h1>
+		</header>
+		<main>
+			<h2>Text:</h2>
+			<p>Bicycle rights schlitz live-edge mixtape fam, beard meditation kinfolk forage. Echo park PBR&B bitters tumblr pickled enamel pin, franzen helvetica tousled everyday carry glossier gluten-free pinterest. Normcore paleo slow-carb synth. Direct trade lomo glossier woke, yr hexagon raclette ethical yuccie aesthetic selfies tumblr street art. Cray franzen butcher kogi kickstarter authentic. Photo booth cold-pressed banh mi forage, knausgaard farm-to-table health goth tilde pork belly offal franzen. Pour-over cold-pressed marfa truffaut, normcore trust fund tumeric synth.
+
+				Drinking vinegar banh mi biodiesel yr, gluten-free snackwave austin artisan echo park chartreuse before they sold out synth ethical chicharrones venmo. Kickstarter lyft scenester tbh helvetica artisan. Cornhole street art brooklyn, typewriter letterpress chambray asymmetrical flexitarian meditation distillery everyday carry hot chicken iPhone. Biodiesel offal green juice photo booth disrupt sriracha, tacos affogato raw denim kale chips waistcoat. Edison bulb church-key selvage vinyl jianbing ugh, food truck 90's thundercats williamsburg mixtape bespoke banh mi. Chia vegan pok pok austin hot chicken jianbing, pinterest gochujang williamsburg hashtag put a bird on it you probably haven't heard of them hell of pour-over af. Pok pok four loko leggings butcher.
+
+				Humblebrag occupy direct trade, thundercats bushwick pop-up ramps lo-fi craft beer la croix man bun. Brooklyn microdosing banjo chicharrones, cray pickled 3 wolf moon keffiyeh. Next level asymmetrical chia, readymade hoodie four loko XOXO pabst iPhone keffiyeh sartorial. IPhone stumptown swag cronut, gluten-free marfa irony prism craft beer farm-to-table four loko intelligentsia vice banjo glossier. Air plant sustainable pop-up, polaroid cardigan unicorn direct trade tacos leggings pickled truffaut man braid you probably haven't heard of them. Skateboard master cleanse typewriter, fam chillwave shoreditch hammock hoodie scenester. You probably haven't heard of them gluten-free mumblecore quinoa, kale chips twee pinterest.
+
+				Air plant keffiyeh blue bottle, kickstarter gluten-free subway tile brooklyn freegan jianbing church-key glossier. Shabby chic bicycle rights selvage, jean shorts cronut yr semiotics pabst sartorial kale chips flexitarian asymmetrical. Listicle squid organic flannel authentic. Bespoke umami crucifix, polaroid scenester jean shorts fap XOXO ethical franzen chicharrones brunch keffiyeh small batch kitsch. Yuccie flannel intelligentsia, lumbersexual crucifix vinyl organic chillwave PBR&B. Pork belly brunch subway tile chambray drinking vinegar, dreamcatcher cronut vinyl chartreuse. Fashion axe art party meh readymade, tofu activated charcoal single-origin coffee.</p>
+			<img src="images/sandbox-normandy-beach.jpg" alt="Sandbox of Normandy Beach" />
+		</main>
 	</body>
 </html>
