fba087ddd20dfb1616cc671507fca986adbe430c
Jeffrey Cooper <jcooper37@cnm.edu>
2017-04-12 08:22:29 -0600

    Add CSS Stylesheet

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>HTML Sandbox</title>
-
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/sandbox-style.css" type="text/css" />
 	</head>
 	<body>
 		<h1>HTML Sandbox</h1>
